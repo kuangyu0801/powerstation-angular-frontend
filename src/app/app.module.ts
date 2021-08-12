b823819e41704d9474faa9881b37270a3264b488
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-08-13 00:42:04 +0200

    Inject LinkQueryService on form submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LinkqueryComponent } from './components/linkquery/linkquery.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LinkqueryComponent } from './components/linkquery/linkquery.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/linkquery/linkquery.component.ts
+++ src/app/components/linkquery/linkquery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { LinkQuery } from 'src/app/common/link-query';
+import { LinkQueryService } from 'src/app/services/link-query.service';
 
 @Component({
   selector: 'app-linkquery',
@@ -9,18 +11,44 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class LinkqueryComponent implements OnInit {
   linkQueryFormGroup!: FormGroup;
   
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private linkQueryService: LinkQueryService) { }
 
   ngOnInit(): void {
     this.linkQueryFormGroup = this.formBuilder.group({
-      linkQuery: this.formBuilder.group({
+      linkQueryForm: this.formBuilder.group({
         x: [],
         y: []
       })
     });
   }
+
   onSubmit() {
     console.log("Handling the submit button");
-    console.log(this.linkQueryFormGroup.get('linkQuery')?.value);
+    console.log(this.linkQueryFormGroup.get('linkQueryForm')?.value);
+    
+    // setting LinkQuery
+    let linkQuery = new LinkQuery();
+    linkQuery.x = this.linkQueryFormGroup.get('linkQuery')?.value.x;
+    linkQuery.y = this.linkQueryFormGroup.get('linkQuery')?.value.y;    
+    
+    // call REST API via LinkQueryService
+    this.linkQueryService.query(linkQuery).subscribe(
+      {
+        next: reponse => {
+          alert(`Link Query has been received`);
+          console.log(reponse.body);
+          //this.resetForm();          
+        },
+        error: err => {
+          alert(`There was an error: ${err.message}`);
+        }
+      }
+    );
   }
+
+  resetForm() {
+    this.linkQueryFormGroup.reset();
+  }
+  
 }
